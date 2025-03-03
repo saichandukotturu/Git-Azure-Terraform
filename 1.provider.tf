546a19e09ca4507efddcbef4a1d384c11fc949e6
saichandukotturu <saichandukotturu@gmail.com>
2025-03-03 10:55:56 -0600

    Git-Azure-Terraform code

---
+++ 1.provider.tf
@@ -0,0 +1,27 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "4.21.1"
+    }
+       aws = {
+      source = "hashicorp/aws"
+      version = "5.89.0"
+    }
+       google = {
+      source = "hashicorp/google"
+      version = "6.23.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features{}
+}
+
+provider "google" {
+  # Configuration options
+}
+provider "aws" {
+  # Configuration options
+}
