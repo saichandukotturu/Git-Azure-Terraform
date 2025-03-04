14ad5e569816cad11b96a367b53105e6efb8faec
saichandukotturu <saichandukotturu@gmail.com>
2025-03-03 18:48:49 -0600

    aded

--- 1.provider.tf
+++ 1.provider.tf
@@ -1,22 +1,23 @@
 terraform {
   required_providers {
     azurerm = {
-      source = "hashicorp/azurerm"
+      source  = "hashicorp/azurerm"
       version = "4.21.1"
     }
-       aws = {
-      source = "hashicorp/aws"
+    aws = {
+      source  = "hashicorp/aws"
       version = "5.89.0"
     }
-       google = {
-      source = "hashicorp/google"
+    google = {
+      source  = "hashicorp/google"
       version = "6.23.0"
     }
   }
 }
 
 provider "azurerm" {
-  features{}
+  features {}
+ 
 }
 
 provider "google" {
