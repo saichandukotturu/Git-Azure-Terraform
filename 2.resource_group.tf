fbd1223e078dfb9dd60c8e2dfd9563ec96ca956d
saichandukotturu <saichandukotturu@gmail.com>
2025-03-05 18:18:05 -0600

    Added Tags

--- 2.resource_group.tf
+++ 2.resource_group.tf
@@ -1,6 +1,10 @@
 resource "azurerm_resource_group" "rg1" {
   name     = "rg1"
   location = "eastus"
+  tags = {
+    Environment = "Production"
+    Project = "Chandu"
+  }
 }
 resource "azurerm_resource_group" "rg2" {
   name     = "rg2"
