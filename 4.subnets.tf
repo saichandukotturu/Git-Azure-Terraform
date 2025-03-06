aadad338377b57982cd9e6169db55a8f5d8f9c10
saichandukotturu <saichandukotturu@gmail.com>
2025-03-05 22:39:35 -0600

    added two more subnets

--- 4.subnets.tf
+++ 4.subnets.tf
@@ -1,12 +1,25 @@
-resource "azurerm_subnet" "subnet1" {
+resource "azurerm_subnet" "vnet1-subnet1" {
   name                 = "${azurerm_virtual_network.vnet1.name}-subnet1"
   resource_group_name  = azurerm_resource_group.rg1.name
   virtual_network_name = azurerm_virtual_network.vnet1.name
   address_prefixes     = ["10.45.1.0/24"]
 }
-resource "azurerm_subnet" "subnet2" {
+resource "azurerm_subnet" "vnet1-subnet2" {
   name                 = "${azurerm_virtual_network.vnet1.name}-subnet2"
   resource_group_name  = azurerm_resource_group.rg1.name
   virtual_network_name = azurerm_virtual_network.vnet1.name
   address_prefixes     = ["10.45.2.0/24"]
+}
+
+resource "azurerm_subnet" "vnet2-subnet1" {
+  name                 = "${azurerm_virtual_network.vnet2.name}-subnet1"
+  resource_group_name  = azurerm_resource_group.rg1.name
+  virtual_network_name = azurerm_virtual_network.vnet2.name
+  address_prefixes     = ["172.16.1.0/24"]
+}
+resource "azurerm_subnet" "vnet2-subnet2" {
+  name                 = "${azurerm_virtual_network.vnet2.name}-subnet2"
+  resource_group_name  = azurerm_resource_group.rg1.name
+  virtual_network_name = azurerm_virtual_network.vnet2.name
+  address_prefixes     = ["172.16.2.0/24"]
 }
